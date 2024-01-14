7f58c4eaf75d95df14da4acb2bee590b6d2f6ac2
Alessio Cocco <ale11022000@gmail.com>
2024-01-14 17:49:38 +0100

    kekko

--- src/main.py
+++ src/main.py
@@ -3,15 +3,25 @@
 import networkit as nk
 import matplotlib.pyplot as plt
 import os
-try:
-    import cugraph as cnx
-    import cudf
-    GPU = True
-except:
-    GPU = False
     
 # Constants
 DATA_PATH = os.getcwd() + "/data/"
+OUTPUT_PATH = os.getcwd() + "/output/"
+
+def degreeCentrality(graph):
+    return nk.centrality.DegreeCentrality(graph).run()
+
+def clusteringCoefficient(graph):
+    graph.removeSelfLoops()
+    return nk.centrality.LocalClusteringCoefficient(graph).run()
+
+def plotMetrics():
+    return
+
+def saveToFile(filename, data):
+    with open(OUTPUT_PATH + filename + '.txt', "w+") as f:
+        for i in range(len(data)):
+            f.write(str(i) + " " + str(data[i]) + "\n")
 
 def main():
     # Dataset selection
@@ -21,26 +31,24 @@ def main():
         print(i, dataset[0])
     index = int(input())
 
+    # Create output folder
+    if not os.path.exists("./output"): os.makedirs("./output")
+
     # Setup graph
-    print("\nGPU    | ", "OK" if GPU else "NO")
     print("START  |  Reading graph", datasets[index][0], "...")
     graph = nk.readGraph(datasets[index][1], nk.Format.EdgeList, separator=' ', firstNode=0, continuous=False)
     print("DONE   |  Graph read!\n")
 
     # 1. Compute degree centrality of each node
     print("START  |  Computing degree centrality...")
-    dc = nk.centrality.DegreeCentrality(graph).run()
-    with open("./output/degree_centrality.txt", "w+") as f:
-        for i in range(graph.numberOfNodes()):
-            f.write(str(i) + " " + str(dc.score(i)) + "\n")
+    dc = degreeCentrality(graph)
+    saveToFile("degree_centrality", dc.scores())
     print("DONE   |  Degree centrality computed\n")
 
     # 2. Compute clustering coefficient of each node
     print("START  |  Computing clustering coefficient...")
-    cc = nk.centrality.LocalClusteringCoefficient(graph).run()
-    with open("./output/clustering_coefficient.txt", "w+") as f:
-        for i in range(graph.numberOfNodes()):
-            f.write(str(i) + " " + str(cc.score(i)) + "\n")
+    cc = clusteringCoefficient(graph)
+    saveToFile("clustering_coefficient", cc.scores())
     print("DONE   |  Clustering coefficient computed\n")
 
     # 3. Plot in scatter plot centrality (x) and clustering coefficient (y)
