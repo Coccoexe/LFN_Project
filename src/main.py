01fe47934ba0bdd587f432a2c9d6595213f7efd4
akappakappa <andreavalde36@gmail.com>
2024-01-25 17:31:04 +0100

    kryegjhksdf

--- src/main.py
+++ src/main.py
@@ -93,40 +93,44 @@ def main():
     # 1. Compute degree centrality of each node
     print("START  |  Computing degree centrality...")
     dc = degreeCentrality(graph)
-    saveToFile(datasets[index][0] + "_degree_centrality", dc.scores())
+    if type(dc) is not list: dc = dc.scores()
+    saveToFile(datasets[index][0] + "_degree_centrality", dc)
     print("DONE   |  Degree centrality computed\n")
 
     # 2. Compute clustering coefficient of each node
     print("START  |  Computing clustering coefficient...")
     cc = clusteringCoefficient(graph)
-    saveToFile(datasets[index][0] + "_clustering_coefficient", cc.scores())
+    if type(cc) is not list: cc = cc.scores()
+    saveToFile(datasets[index][0] + "_clustering_coefficient", cc)
     print("DONE   |  Clustering coefficient computed\n")
 
     # 5. Compute eigen centrality of each node
     print("START  |  Computing eigen centrality...")
     ec = eigenCentrality(graph)
-    saveToFile(datasets[index][0] + "_eigen_centrality", ec.scores())
-    plotHistogram(datasets[index][0] + "ECHist", ec.scores(), "Eigen Centrality")
+    if type(ec) is not list: ec = ec.scores()
+    saveToFile(datasets[index][0] + "_eigen_centrality", ec)
+    plotHistogram(datasets[index][0] + "ECHist", ec, "Eigen Centrality")
     print("DONE   |  Eigen centrality computed\n")
 
     # 6. Compute betweenness centrality of each node
     print("START  |  Computing betweenness centrality...")
     bc = betweennessCentrality(graph)
-    saveToFile(datasets[index][0] + "_betweenness_centrality", bc.scores())
-    plotHistogram(datasets[index][0] + "BCHist", bc.scores(), "Betweenness Centrality")
+    if type(bc) is not list: bc = bc.scores()
+    saveToFile(datasets[index][0] + "_betweenness_centrality", bc)
+    plotHistogram(datasets[index][0] + "BCHist", bc, "Betweenness Centrality")
     print("DONE   |  Betweenness centrality computed\n")
 
     # 3. Plot in scatter plot centrality (x) and clustering coefficient (y)
     print("START  |  Plotting combined metrics...")
-    plotCombinedMetrics(datasets[index][0], dc.scores(), cc.scores())
+    plotCombinedMetrics(datasets[index][0], dc, cc)
     print("DONE   |  Metrics PLot saved to file\n")
 
     # 4. Plot in histogram the distribution of centrality and clustering coefficient separately
     print("START  |  Plotting degree centrality histogram...")
-    plotHistogram(datasets[index][0] + "DCHist", dc.scores(), "Degree Centrality")
+    plotHistogram(datasets[index][0] + "DCHist", dc, "Degree Centrality")
     print("DONE   |  Degree centrality histogram saved to file\n")
     print("START  |  Plotting clustering coefficient histogram...")
-    plotHistogram(datasets[index][0] + "CCHist", cc.scores(), "Clustering Coefficient")
+    plotHistogram(datasets[index][0] + "CCHist", cc, "Clustering Coefficient")
     print("DONE   |  Clustering coefficient histogram saved to file\n")
     
 if __name__ == "__main__":
