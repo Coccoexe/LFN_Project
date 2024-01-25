aed47cdb9d3f97c64fb8f47fc9de45ea77c4ec8f
akappakappa <andreavalde36@gmail.com>
2024-01-25 17:25:21 +0100

    pushala v2 kekw amongus

--- src/main.py
+++ src/main.py
@@ -3,17 +3,33 @@
 import networkit as nk
 import matplotlib.pyplot as plt
 import os
-    
+
 # Constants
 DATA_PATH = os.getcwd() + "/data/"
 OUTPUT_PATH = os.getcwd() + "/output/"
+skip = True
+dataset = None
 
 def degreeCentrality(graph):
+    if skip and os.path.exists(OUTPUT_PATH + dataset[0] + "_degree_centrality.txt"):
+        return loadFromFile(dataset[0] + "_degree_centrality")
     return nk.centrality.DegreeCentrality(graph).run()
 
 def clusteringCoefficient(graph):
+    if skip and os.path.exists(OUTPUT_PATH + dataset[0] + "_clustering_coefficient.txt"):
+        return loadFromFile(dataset[0] + "_clustering_coefficient")
     graph.removeSelfLoops()
     return nk.centrality.LocalClusteringCoefficient(graph).run()
+
+def betweennessCentrality(graph):
+    if skip and os.path.exists(OUTPUT_PATH + dataset[0] + "_betweenness_centrality.txt"):
+        return loadFromFile(dataset[0] + "_betweenness_centrality")
+    return nk.centrality.Betweenness(graph).run()
+
+def eigenCentrality(graph):
+    if skip and os.path.exists(OUTPUT_PATH + dataset[0] + "_eigen_centrality.txt"):
+        return loadFromFile(dataset[0] + "_eigen_centrality")
+    return nk.centrality.EigenvectorCentrality(graph).run()
 
 def plotCombinedMetrics(name, degrees, clustering):
     plt.scatter(degrees, clustering, s = 0.1)
@@ -39,7 +55,16 @@ def saveToFile(filename, data):
         for i in range(len(data)):
             f.write(str(i) + " " + str(data[i]) + "\n")
 
+def loadFromFile(filename):
+    data = []
+    with open(OUTPUT_PATH + filename + '.txt', "r") as f:
+        for line in f:
+            data.append(float(line.split()[1]))
+    return data
+
 def main():
+    global skip
+    global dataset
 
     if not os.path.exists(DATA_PATH):
         print("ERROR  |  Data folder not found. Please create a folder named 'data' and put the datasets inside it.")
@@ -51,13 +76,18 @@ def main():
     for i, dataset in enumerate(datasets):
         print(i, dataset[0])
     index = int(input())
+    dataset = datasets[index]
+
+    print("\nWant to force recomputation of metrics? (y/N)")
+    input_ = input()
+    if input_ == 'y' or input_ == 'Y': skip = False
 
     # Create output folder
     if not os.path.exists("./output"): os.makedirs("./output")
 
     # Setup graph
     print("START  |  Reading graph", datasets[index][0], "...")
-    graph = nk.readGraph(datasets[index][1], nk.Format.EdgeList, separator=' ', firstNode=0, continuous=False)
+    graph = nk.readGraph(datasets[index][1], nk.Format.EdgeList, separator = ' ', firstNode = 0, continuous = False)
     print("DONE   |  Graph read!\n")
 
     # 1. Compute degree centrality of each node
@@ -72,6 +102,20 @@ def main():
     saveToFile(datasets[index][0] + "_clustering_coefficient", cc.scores())
     print("DONE   |  Clustering coefficient computed\n")
 
+    # 5. Compute eigen centrality of each node
+    print("START  |  Computing eigen centrality...")
+    ec = eigenCentrality(graph)
+    saveToFile(datasets[index][0] + "_eigen_centrality", ec.scores())
+    plotHistogram(datasets[index][0] + "ECHist", ec.scores(), "Eigen Centrality")
+    print("DONE   |  Eigen centrality computed\n")
+
+    # 6. Compute betweenness centrality of each node
+    print("START  |  Computing betweenness centrality...")
+    bc = betweennessCentrality(graph)
+    saveToFile(datasets[index][0] + "_betweenness_centrality", bc.scores())
+    plotHistogram(datasets[index][0] + "BCHist", bc.scores(), "Betweenness Centrality")
+    print("DONE   |  Betweenness centrality computed\n")
+
     # 3. Plot in scatter plot centrality (x) and clustering coefficient (y)
     print("START  |  Plotting combined metrics...")
     plotCombinedMetrics(datasets[index][0], dc.scores(), cc.scores())
@@ -84,7 +128,6 @@ def main():
     print("START  |  Plotting clustering coefficient histogram...")
     plotHistogram(datasets[index][0] + "CCHist", cc.scores(), "Clustering Coefficient")
     print("DONE   |  Clustering coefficient histogram saved to file\n")
-
+    
 if __name__ == "__main__":
     main()
-
